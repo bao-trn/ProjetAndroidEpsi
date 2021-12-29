0a7f39915f8476b88e65a0b07f4e2bc42585152e
bao <bao_trn@outlook.fr>
2021-12-29 12:46:27 +0100

    changed Toast

--- app/src/main/java/fr/epsi/epsig2/AppRenduEPSI.kt
+++ app/src/main/java/fr/epsi/epsig2/AppRenduEPSI.kt
@@ -3,7 +3,7 @@ package fr.epsi.epsig2
 import android.app.Application
 import android.widget.Toast
 
-class AppEpsi : Application(){
+class AppRenduEPSI : Application(){
 
     fun showToast(txt : String){
         Toast.makeText(this,txt,Toast.LENGTH_SHORT).show()
